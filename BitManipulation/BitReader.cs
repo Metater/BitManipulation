1148a241d805974e860c25e176694b221873c5ae
Connor <61102529+Metater@users.noreply.github.com>
2021-05-19 23:05:06 -0400

    Add project

---
+++ BitManipulation/BitReader.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BitManipulation
+{
+    public class BitReader
+    {
+        private byte[] data = new byte[0];
+        private int readPos = 0;
+
+        private ulong scratch = 0;
+        private int scratchIndex = 0;
+
+        public BitReader(byte[] data)
+        {
+
+        }
+    }
+}
