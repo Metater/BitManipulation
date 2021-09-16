c9d7d48bc0e9ec319e796ceb22eb286da31e1b20
Connor <61102529+Metater@users.noreply.github.com>
2021-09-15 22:10:23 -0400

    Reader

---
+++ BitManipulation/BitReader.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BitManipulation
+{
+    public class BitReader
+    {
+        private byte[] data;
+        private int readPos = 0;
+
+        private ulong scratch = 0;
+        private int scratchIndex = 0;
+
+        public BitReader(byte[] data)
+        {
+            this.data = data;
+        }
+
+        public bool ReadBit()
+        {
+            CheckData();
+            bool value = (scratch & (1UL << scratchIndex)) != 0;
+            scratchIndex--;
+            return value;
+        }
+
+        public int ReadInt(int bits = 32)
+        {
+            CheckData();
+            int bitsToDrop = 32 - bits;
+            int value = (int)scratch << bitsToDrop;
+            value >>= bitsToDrop;
+            scratch >>= bits;
+            scratchIndex -= bits;
+            return value;
+        }
+
+        private void CheckData()
+        {
+            if (scratchIndex <= 32)
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    if (readPos >= data.Length) break;
+                    scratch |= ((ulong)data[readPos + i] << scratchIndex);
+                    scratchIndex += 8;
+                }
+                readPos += 4;
+            }
+        }
+    }
+}
--- BitManipulationTesting/Program.cs
+++ BitManipulationTesting/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using BitManipulation;
 
 namespace BitManipulationTesting
@@ -8,11 +9,47 @@ namespace BitManipulationTesting
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            BitWriterOld bitWriter = new BitWriterOld();
-            bitWriter.ProvisionBytes(1);
+            BitWriter bitWriter = new BitWriter(8);
+            /*
             bitWriter.WriteByte(127, 6);
+            bitWriter.WriteBit(true);
+            bitWriter.WriteBit(true);
+            bitWriter.WriteInt(0xff, 24);
+            bitWriter.WriteBit(true);
+            bitWriter.WriteByte(0x55, 7);
+            bitWriter.WriteByte(0x55);
+            bitWriter.WriteByte(0x55);
+            bitWriter.WriteByte(0x55);
+            */
+            bitWriter.WriteInt(1431655765, 31);
+            bitWriter.WriteBit(true);
+            Console.WriteLine(bitWriter.scratch);
+            //bitWriter.WriteLong(1L << 63, 64);
             byte[] data = bitWriter.Assemble();
-            BitWriterOld.PrintByteArray(data);
+            foreach (byte b in data)
+            {
+                Console.WriteLine(b);
+            }
+            Console.WriteLine("--------------------------");
+            BitWriter.PrintBits(data);
+            BitReader bitReader = new BitReader(data);
+            Console.WriteLine(bitReader.ReadInt(31));
+            Console.WriteLine(bitReader.ReadBit());
+            //Console.WriteLine(bitReader.ReadInt());
+            /*
+            for (int i = 0; i < data.Length; i++)
+            {
+                string line = "";
+                for (int j = 0; j < 8; j++)
+                {
+                    if (bitReader.ReadBit())
+                        line += "1";
+                    else
+                        line += "0";
+                }
+                Console.WriteLine(line);
+            }
+            */
         }
     }
 }
